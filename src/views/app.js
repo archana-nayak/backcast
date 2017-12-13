b7fffb5feb2512fd06cee2704b7646794fe37d7e
archana <archana.nayak@gmail.com>
2017-12-12 23:17:10 -0800

    Add video list and video list entry display

--- src/views/app.js
+++ src/views/app.js
@@ -3,15 +3,21 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(exampleVideoData);
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    new VideoListView(
+    	{el: this.$('.list'),
+    	 collection: this.videos	
+    	}); 
     return this;
   },
 
   template: templateURL('src/templates/app.html')
+
 
 });
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,36 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+  	this.render();
+  },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    this.collection.each(this.renderVideo,this);
     return this;
+    
+  },
+
+  renderVideo: function(video) {
+    this.$('.video-list').append(
+    	new VideoListEntryView({model: video}).render().el);
   },
 
   template: templateURL('src/templates/videoList.html')
 
 });
+
+ 
+
+// this.collection.each(function ( page ) { 
+//     this.$el.append( new PageView({ model: page }).render().el );
+// }, this);
+
+//Tidbit of code from the net 
+//source https://coderwall.com/p/bfob4w/this-collection-each-accepts-a-context-object-backbone
+
+//this here refers to the html element({el :'.list'}) that this view
+    //has been initialized to. In this case, in app.js, we initialized it
+    //with '.list' of the newly created html elements in the 
+    //app.html body
